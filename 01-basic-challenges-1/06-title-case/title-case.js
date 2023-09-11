a8c4c831f977e21dc8f1d36e63000ffda4bdf629
Thomas <tholmes59@gmail.com>
2023-09-11 16:28:52 -0700

    solved title case challange

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,12 @@
-function titleCase() {}
+function titleCase(str) {
+  let strArr = str.split(" ");
+  let finArr = [];
+  strArr.forEach((element) => {
+    finArr.push(
+      element.charAt(0).toUpperCase() + element.slice(1).toLowerCase()
+    );
+  });
+  return finArr.join(" ");
+}
 
 module.exports = titleCase;
