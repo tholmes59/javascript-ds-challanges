5494f817b1099fa49fcdb8b027a48126bf9ee987
Thomas <tholmes59@gmail.com>
2023-09-11 14:27:24 -0700

    solve calculator challange

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,16 @@
-function calculator() {}
+function calculator(a, b, c) {
+  switch (c) {
+    case "+":
+      return a + b;
+    case "-":
+      return a - b;
+    case "*":
+      return a * b;
+    case "/":
+      return a / b;
+    default:
+      throw new Error("Invalid operator");
+  }
+}
 
 module.exports = calculator;
