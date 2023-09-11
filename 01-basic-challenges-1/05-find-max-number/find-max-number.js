e496087ab4d272b024025933aa03b66356bd16f5
Thomas <tholmes59@gmail.com>
2023-09-11 14:50:06 -0700

    solved find max nimber challange

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,11 @@
-function findMaxNumber() {}
+function findMaxNumber(arr) {
+  let max = arr[0];
+  arr.forEach((el) => {
+    if (el > max) {
+      max = el;
+    }
+  });
+  return max;
+}
 
 module.exports = findMaxNumber;
