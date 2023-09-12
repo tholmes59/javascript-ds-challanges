8fbca7a2fac077ad9c30b623d0168f20fa5633d6
Thomas <tholmes59@gmail.com>
2023-09-12 09:42:47 -0700

    solved palindrome challange

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,21 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+  let revStr = str
+    .toLowerCase()
+    .replace(/\s/g, "")
+    .replace(/[^a-z0-9]/g, "")
+    .split("")
+    .reverse()
+    .join("");
+  let upStr = str
+    .toLowerCase()
+    .replace(/\s/g, "")
+    .replace(/[^a-z0-9]/g, "");
+
+  if (upStr === revStr) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 module.exports = isPalindrome;
