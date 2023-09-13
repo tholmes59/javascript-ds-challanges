c79f1ad8e5aa9e3feb6819955f7d6f1351ff4ce2
Thomas <tholmes59@gmail.com>
2023-09-13 13:48:24 -0700

    solved find missing letter challange

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,12 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+  const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  let startInx = alphabet.indexOf(arr[0]);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== alphabet[startInx + i]) {
+      return alphabet[startInx + i];
+    }
+  }
+}
 
 module.exports = findMissingLetter;
