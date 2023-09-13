f4ec20c3f06413c6bc16e1149d5d1163e4e1a352
Thomas <tholmes59@gmail.com>
2023-09-13 10:46:53 -0700

    solved display likes challange

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,19 @@
-function displayLikes() {}
+function displayLikes(arr) {
+  if (arr.length === 0) {
+    return "no one likes this";
+  }
+  if (arr.length === 1) {
+    return `${arr[0]} likes this`;
+  }
+  if (arr.length === 2) {
+    return `${arr[0]} and ${arr[1]} like this`;
+  }
+  if (arr.length === 3) {
+    return `${arr[0]}, ${arr[1]} and ${arr[2]} like this`;
+  }
+  if (arr.length > 3) {
+    return `${arr[0]}, ${arr[1]} and ${arr.length - 2} others like this`;
+  }
+}
 
 module.exports = displayLikes;
