4dac89f0c8c0aa79f2eae3fa97dd770ab571d071
Thomas <tholmes59@gmail.com>
2023-09-13 08:18:43 -0700

    solved array intersection challange

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,13 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+  let interArr = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr2.includes(arr1[i]) && !interArr.includes(arr1[i])) {
+      interArr.push(arr1[i]);
+    }
+  }
+
+  return interArr;
+}
 
 module.exports = arrayIntersection;
