7668583a297594a137762a883eef05b92182b790
Thomas <tholmes59@gmail.com>
2023-09-13 11:31:21 -0700

    solved find missing number challange

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,4 +1,6 @@
 function findMissingNumber(arr) {
+  if (arr.length === 0) return 1;
+  if (!arr) return undefined;
   let sortArr = arr.sort((a, b) => a - b);
   let nVal = sortArr.length + 1;
   let sumOfNum = (nVal * (nVal + 1)) / 2;
