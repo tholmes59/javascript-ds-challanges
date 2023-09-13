693b17b1dde2f40d9448de5e089fb935471970d6
Thomas <tholmes59@gmail.com>
2023-09-13 14:10:04 -0700

    solved unique characters challange

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,10 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+  if (str.length === 0) return true;
+
+  let strArr = str.split("");
+  let removeDupArr = Array.from(new Set(strArr));
+
+  return strArr.length === removeDupArr.length ? true : false;
+}
 
 module.exports = areAllCharactersUnique;
