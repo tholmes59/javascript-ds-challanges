30590ef94ac16fb8f8e2fefae78bf53795a0d36f
Thomas <tholmes59@gmail.com>
2023-09-12 19:24:44 -0700

    solve fizzbuzz challange

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,17 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(num) {
+  let finArr = [];
+  for (let i = 1; i < num + 1; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      finArr.push("FizzBuzz");
+    } else if (i % 5 === 0) {
+      finArr.push("Buzz");
+    } else if (i % 3 === 0) {
+      finArr.push("Fizz");
+    } else {
+      finArr.push(i);
+    }
+  }
+  return finArr;
+}
 
 module.exports = fizzBuzzArray;
