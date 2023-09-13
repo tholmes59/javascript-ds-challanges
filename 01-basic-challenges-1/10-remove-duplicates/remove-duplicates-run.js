2c207e7011f39add7ad16223820c9a7cf535c674
Thomas <tholmes59@gmail.com>
2023-09-12 18:29:31 -0700

    solved remove duplicates challange

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates-run.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates-run.js
@@ -1,20 +1,5 @@
-const removeDuplicates = require('./remove-duplicates');
+const removeDuplicates = require("./remove-duplicates");
 
-const result = removeDuplicates([
-  1,
-  2,
-  3,
-  4,
-  5,
-  5,
-  5,
-  6,
-  7,
-  8,
-  'hello',
-  'hello',
-  true,
-  true,
-]);
+const result = removeDuplicates([1, 2, 3, 2, 4, 1, 5]);
 
 console.log(result);
