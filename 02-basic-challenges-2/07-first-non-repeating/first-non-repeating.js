56c221b5829d8dbe6a70b7f9221623db7366534a
Thomas <tholmes59@gmail.com>
2023-09-13 14:46:02 -0700

    solved unique first non repeating challange

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,17 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(str) {
+  let charObj = {};
+
+  for (let i = 0; i < str.length; i++) {
+    charObj[str[i]] = (charObj[str[i]] || 0) + 1;
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    if (charObj[str[i]] === 1) {
+      return str[i];
+    }
+  }
+
+  return null;
+}
 
 module.exports = findFirstNonRepeatingCharacter;
