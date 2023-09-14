5736b11b4cc2b2d4c086d867fae72d8ce55cbb41
Thomas <tholmes59@gmail.com>
2023-09-14 09:41:28 -0700

    solved dice chaallange

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,21 @@
-function diceGameSimulation() {}
+function diceGameSimulation(num) {
+  let results = [];
+
+  for (let i = 0; i < num; i++) {
+    let dice1 = Math.floor(Math.random() * 6) + 1;
+    let dice2 = Math.floor(Math.random() * 6) + 1;
+    let sum = dice1 + dice2;
+    let result;
+    if (sum === 7 || sum === 11) {
+      result = "win";
+    } else if (sum === 2 || sum === 3 || sum === 12) {
+      result = "lose";
+    } else {
+      result = "roll again";
+    }
+    results.push({ dice1, dice2, sum, result });
+  }
+  return results;
+}
 
 module.exports = diceGameSimulation;
