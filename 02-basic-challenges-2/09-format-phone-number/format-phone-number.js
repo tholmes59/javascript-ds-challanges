6deb89e09a0f3654ed8b6558636ea9fffc2493dc
Thomas <tholmes59@gmail.com>
2023-09-14 11:02:32 -0700

    solved phone number challange

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,12 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(arr) {
+  let firstPar = "(";
+  let secondPar = ")";
+
+  let areaCode = arr.slice(0, 3).join("");
+  let middleNum = arr.slice(3, 6).join("");
+  let endNum = arr.slice(6).join("");
+
+  return firstPar + areaCode + secondPar + " " + middleNum + "-" + endNum;
+}
 
 module.exports = formatPhoneNumber;
