50bf6198422b6d584676e7a1ec4b25254e7e23a9
Thomas <tholmes59@gmail.com>
2023-09-15 09:27:02 -0700

    solved calcualte sales challange

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,11 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(arr, perct) {
+  let sales = 0;
+
+  arr.map((val) => {
+    sales += val.quantity * val.price;
+  });
+
+  return (1 + perct / 100) * sales;
+}
 
 module.exports = calculateTotalSalesWithTax;
