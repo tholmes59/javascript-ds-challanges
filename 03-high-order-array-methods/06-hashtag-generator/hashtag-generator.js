ece08d14c9a8a3f1e544057e68dc210faa7d74a2
Thomas <tholmes59@gmail.com>
2023-09-15 16:04:46 -0700

    solved hashtag generator challange

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,13 @@
-function generateHashtag() {}
+function generateHashtag(str) {
+  if (str.length > 140 || str.length === 0) return false;
+
+  let strArr = str.split(" ").map((word) => {
+    let cap = word.charAt(0).toUpperCase() + word.slice(1);
+    return cap;
+  });
+
+  strArr.unshift("#");
+  return strArr.join("");
+}
 
 module.exports = generateHashtag;
