2e1c76a37edf19a472fa7fa8f2e68e419dc4eba6
Thomas <tholmes59@gmail.com>
2023-09-15 14:11:45 -0700

    solved highest scoring word challange

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,3 +1,17 @@
-function highestScoringWord() {}
+function highestScoringWord(str) {
+  let wordArr = str.split(" ");
+
+  let splitWord = wordArr.map((word) => {
+    return word
+      .split("")
+      .reduce((score, letter) => score + letter.charCodeAt(0) - 96, 0);
+  });
+  console.log(splitWord);
+
+  const highestScore = Math.max(...splitWord);
+  const highestIndex = splitWord.indexOf(highestScore);
+
+  return wordArr[highestIndex];
+}
 
 module.exports = highestScoringWord;
