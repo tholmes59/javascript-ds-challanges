8bbcb16e14512b590fdd89dfc246f875fbd33168
Thomas <tholmes59@gmail.com>
2023-09-15 15:03:11 -0700

    solved anagrams challange

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,15 @@
-function validAnagrams() {}
+function validAnagrams(str1, str2) {
+  let srt1Arr = str1.split("").reduce((acc, item) => {
+    acc[item] = acc[item] ? acc[item] + 1 : 1;
+    return acc;
+  }, {});
+
+  let srt2Arr = str2.split("").reduce((acc, item) => {
+    acc[item] = acc[item] ? acc[item] + 1 : 1;
+    return acc;
+  }, {});
+
+  return Object.keys(srt1Arr).every((char) => srt1Arr[char] === srt2Arr[char]);
+}
 
 module.exports = validAnagrams;
