ec2849116ee3065f9fab29ecb14145c2f0460a3d
Thomas <tholmes59@gmail.com>
2023-09-14 17:41:43 -0700

    solved sum of even squares challange

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,15 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(arr) {
+  if (arr.length === 0) return 0;
+  return arr
+    .filter((num) => {
+      return num % 2 === 0;
+    })
+    .map((num) => {
+      return num * num;
+    })
+    .reduce((a, b) => {
+      return a + b;
+    });
+}
 
 module.exports = sumOfEvenSquares;
