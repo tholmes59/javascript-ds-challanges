101b813d911d310cec17d10921937aa073fabe5e
Thomas <tholmes59@gmail.com>
2023-09-18 15:54:32 -0700

    solved ivp4 challange

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,3 +1,11 @@
-const isValidIPv4 = () => {};
+const isValidIPv4 = (str) => {
+  let arr = str.split(".");
+
+  if (arr.length !== 4) return false;
+
+  let thrshold = (val) => +val.split("")[0] !== 0 && +val >= 0 && +val <= 255;
+
+  return arr.every(thrshold);
+};
 
 module.exports = isValidIPv4;
