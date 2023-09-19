d6dd6493085153628caf12cd0e7f2a47490015f7
Thomas <tholmes59@gmail.com>
2023-09-19 10:34:04 -0700

    solved password validator challange

--- 03-high-order-array-methods/09-password-validator/password-validator.js
+++ 03-high-order-array-methods/09-password-validator/password-validator.js
@@ -1,3 +1,11 @@
-function validatePassword() {}
+function validatePassword(str) {
+  if (!str.length >= 8) return false;
+
+  let pattern = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)");
+
+  if (!pattern.test(str)) return false;
+
+  return true;
+}
 
 module.exports = validatePassword;
