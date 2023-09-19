633aa5f84ca8748b9c982f17e9386801e84371d6
Thomas <tholmes59@gmail.com>
2023-09-18 17:22:40 -0700

    solved car mileage challange

--- 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,3 +1,35 @@
-function analyzeCarMileage() {}
+function analyzeCarMileage(arr) {
+  let avgMileage = (
+    arr.reduce((a, b) => a + b.mileage, 0) / arr.length
+  ).toFixed(2);
+  let totalMileage = arr.reduce((a, b) => a + b.mileage, 0).toFixed(2);
+
+  let highestMileage = 0;
+  let highest;
+
+  arr.forEach((element) => {
+    if (element.mileage > highestMileage) {
+      highestMileage = element.mileage;
+      highest = element;
+    }
+  });
+
+  let lowestMileage = 1000000;
+  let lowest;
+
+  arr.forEach((element) => {
+    if (element.mileage < lowestMileage) {
+      lowestMileage = element.mileage;
+      lowest = element;
+    }
+  });
+
+  return {
+    averageMileage: +avgMileage,
+    highestMileageCar: highest,
+    lowestMileageCar: lowest,
+    totalMileage: +totalMileage,
+  };
+}
 
 module.exports = analyzeCarMileage;
