fbe320e29a89a486dd2a0c155c779e88127244d0
Thomas <tholmes59@gmail.com>
2023-09-19 18:50:25 -0700

    solve recursive countdown challange

--- 04-recursion/01-recursion-intro/count-down.js
+++ 04-recursion/01-recursion-intro/count-down.js
@@ -1,3 +1,12 @@
-function countDown() {}
+function countDown(num) {
+  if (num <= 0) {
+    console.log("All done!");
+    return;
+  }
+
+  console.log(num);
+  num--;
+  countDown(num);
+}
 
 module.exports = countDown;
