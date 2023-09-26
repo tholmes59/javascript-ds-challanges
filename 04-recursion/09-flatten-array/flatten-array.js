5babce1c0421fa55537db270da65a56080a23273
Thomas <tholmes59@gmail.com>
2023-09-26 13:30:50 -0700

    solve flatten array recursion challange

--- 04-recursion/09-flatten-array/flatten-array.js
+++ 04-recursion/09-flatten-array/flatten-array.js
@@ -1,3 +1,7 @@
-function flattenArray() {}
+function flattenArray(arr) {
+  let flatArr = arr.flat();
+  if (flatArr.length === arr.length) return flatArr;
+  return flattenArray(flatArr);
+}
 
 module.exports = flattenArray;
