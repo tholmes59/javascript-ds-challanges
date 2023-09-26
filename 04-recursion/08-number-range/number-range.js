8be8066994c0bf266c2fd550e13a6349bdcd85bc
Thomas <tholmes59@gmail.com>
2023-09-26 12:50:43 -0700

    solved number range recursion challange

--- 04-recursion/08-number-range/number-range.js
+++ 04-recursion/08-number-range/number-range.js
@@ -1,3 +1,9 @@
-function numberRange() {}
+function numberRange(start, end) {
+  if (start === end) return [start];
+
+  const numbers = numberRange(start, end - 1);
+  numbers.push(end);
+  return numbers;
+}
 
 module.exports = numberRange;
