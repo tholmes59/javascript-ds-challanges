e4de026e574e182fa44fa1abc7865eb18a35f2cc
Thomas <tholmes59@gmail.com>
2023-09-27 09:24:06 -0700

    complete word frequesncy counter challange

--- 06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
+++ 06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
@@ -1,3 +1,19 @@
-function wordFrequencyCounter() {}
+function wordFrequencyCounter(str) {
+  const noPeriod = str.toLowerCase().split(/\W+/);
+
+  const wordMap = new Map();
+
+  for (const word of noPeriod) {
+    if (word === "") continue;
+
+    if (wordMap.has(word)) {
+      wordMap.set(word, wordMap.get(word) + 1);
+    } else {
+      wordMap.set(word, 1);
+    }
+  }
+
+  return wordMap;
+}
 
 module.exports = wordFrequencyCounter;
