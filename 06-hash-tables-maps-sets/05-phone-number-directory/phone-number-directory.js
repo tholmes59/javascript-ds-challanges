34d55d810c60f5471906125246240a8158753479
Thomas <tholmes59@gmail.com>
2023-09-28 11:11:39 -0700

    solve phone number directory challange

--- 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
+++ 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
@@ -1,3 +1,12 @@
-function phoneNumberDirectory() {}
+function phoneNumberDirectory(arr) {
+  let phonenumberArr = arr.map((phoneNum) => {
+    return phoneNum.split(":");
+  });
+
+  //   console.log(phonenumberArr);
+  let phoneMap = new Map(phonenumberArr);
+  return phoneMap;
+  //   console.log(phoneMap);
+}
 
 module.exports = phoneNumberDirectory;
