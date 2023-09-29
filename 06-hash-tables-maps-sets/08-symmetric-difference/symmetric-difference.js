0e3eb652e5fff34b9a17b5288e31b277828b8d8e
Thomas <tholmes59@gmail.com>
2023-09-29 14:56:58 -0700

    solve symmantic difference challange

--- 06-hash-tables-maps-sets/08-symmetric-difference/symmetric-difference.js
+++ 06-hash-tables-maps-sets/08-symmetric-difference/symmetric-difference.js
@@ -1,3 +1,20 @@
-function symmetricDifference() {}
+function symmetricDifference(arr1, arr2) {
+  let set1 = new Set(arr1);
+  let set2 = new Set(arr2);
+
+  let diffArr = [];
+
+  for (let num of set1) {
+    if (!set2.has(num)) {
+      diffArr.push(num);
+    }
+  }
+  for (let num of set2) {
+    if (!set1.has(num)) {
+      diffArr.push(num);
+    }
+  }
+  return diffArr;
+}
 
 module.exports = symmetricDifference;
