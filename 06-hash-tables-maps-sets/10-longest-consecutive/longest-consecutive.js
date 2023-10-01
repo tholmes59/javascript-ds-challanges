3921886ea393f4aac492582eb5cb0f258afa9d3e
Thomas <tholmes59@gmail.com>
2023-10-01 15:56:39 -0700

    solve longest sum challange

--- 06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive.js
+++ 06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive.js
@@ -1,3 +1,23 @@
-function longestConsecutiveSequence() {}
+function longestConsecutiveSequence(arr) {
+  const numSet = new Set(nums);
+
+  let longestSequence = 0;
+
+  for (const num of numSet) {
+    if (!numSet.has(num - 1)) {
+      let currentNum = num;
+      let currentSequence = 1;
+
+      while (numSet.has(currentNum + 1)) {
+        currentNum++;
+        currentSequence++;
+      }
+
+      longestSequence = Math.max(longestSequence, currentSequence);
+    }
+  }
+
+  return longestSequence;
+}
 
 module.exports = longestConsecutiveSequence;
