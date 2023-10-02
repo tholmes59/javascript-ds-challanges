32839ced4849bdb4aa7f5cf9fdf6ceab04088aba
Thomas <tholmes59@gmail.com>
2023-10-02 15:18:28 -0700

    create hash map challange

--- 06-hash-tables-maps-sets/11-custom-hash-table/custom-hash-table-run.js
+++ 06-hash-tables-maps-sets/11-custom-hash-table/custom-hash-table-run.js
@@ -1,1 +1,14 @@
-const HashTable = require('./custom-hash-table');
+const HashTable = require("./custom-hash-table");
+
+const myHashTable = new HashTable();
+
+myHashTable.set("John", "555-234-3455");
+myHashTable.set("James", "555-349-3948");
+myHashTable.set("Sara", "555-469-3334");
+
+myHashTable.remove("James");
+
+// console.log(myHashTable.has("John"));
+// console.log(myHashTable.has("James"));
+myHashTable.clear();
+myHashTable.printTable();
