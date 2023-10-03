208aa3e38f977bf443db2146b60c01f40178929b
Thomas <tholmes59@gmail.com>
2023-10-03 10:31:46 -0700

    solve word instance count hash map challange

--- 06-hash-tables-maps-sets/12-word-instance-counter/word-instance-counter.js
+++ 06-hash-tables-maps-sets/12-word-instance-counter/word-instance-counter.js
@@ -1,5 +1,28 @@
-const HashTable = require('./HashTable');
+const HashTable = require("./HashTable");
 
-function wordInstanceCounter() {}
+function wordInstanceCounter(str, word) {
+  let hashTable = new HashTable();
+
+  let lowercaseWordArr = str.toLowerCase().split(/\W+/);
+  let lowercaseTargetWord = word.toLowerCase();
+
+  let count = 0;
+
+  for (const currentWord of lowercaseWordArr) {
+    if (currentWord === "") continue;
+
+    if (hashTable.has(currentWord)) {
+      hashTable.set(currentWord, hashTable.get(currentWord) + 1);
+    } else {
+      hashTable.set(currentWord, 1);
+    }
+
+    if (currentWord === lowercaseTargetWord) {
+      count = hashTable.get(currentWord);
+    }
+  }
+
+  return count;
+}
 
 module.exports = wordInstanceCounter;
