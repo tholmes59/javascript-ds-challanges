a4be69f7809f0b1e8df4da976373c059ac9e2559
Thomas <tholmes59@gmail.com>
2023-10-04 13:31:55 -0700

    solve anagram groupings hash map challange

--- 06-hash-tables-maps-sets/14-custom-anagram-grouping/custom-anagram-grouping.js
+++ 06-hash-tables-maps-sets/14-custom-anagram-grouping/custom-anagram-grouping.js
@@ -1,5 +1,18 @@
-const HashTable = require('./HashTable');
+const HashTable = require("./HashTable");
 
-function anagramGrouping() {}
+function anagramGrouping(arr) {
+  let wordHashTable = new HashTable();
+
+  for (let i = 0; i < arr.length; i++) {
+    let sortWord = arr[i].split("").sort().join("");
+    if (wordHashTable.get(sortWord)) {
+      wordHashTable.get(sortWord).push(arr[i]);
+    } else {
+      wordHashTable.set(sortWord, [arr[i]]);
+    }
+  }
+
+  return wordHashTable.getValues();
+}
 
 module.exports = anagramGrouping;
